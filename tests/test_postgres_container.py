4a0005e937e55c4b766167daeb113ce9441622f2
bsoniam <soniam.bogos@gmail.com>
2018-04-10 13:02:23 +0200

    retesting + cleaning

--- tests/test_postgres_container.py
+++ tests/test_postgres_container.py
@@ -20,7 +20,7 @@ logging.basicConfig(
     datefmt='%m/%d/%Y %I:%M:%S %p'
 )
 logger = logging.getLogger("postgres_tools.tests.test_postgres_container")
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.DEBUG)
 
 
 @pytest.mark.usefixtures('psql_settings', 'settings', scope='class')
@@ -37,6 +37,7 @@ class TestContainerPostgresql():
         """
 
         container_name = settings['container_name']
+
         command_postgresql = (
         "busctl", "get-property", "org.freedesktop.systemd1", "/org/freedesktop/systemd1/unit/postgresql_2eservice",
         "org.freedesktop.systemd1.Unit", "ActiveState")
@@ -48,6 +49,7 @@ class TestContainerPostgresql():
 
         # check the return value
         postgresql_status = check_service().stdout.decode("utf-8")
+        logger.debug(postgresql_status)
 
         status = re.search(active_status, postgresql_status)
         assert status is not None
@@ -60,6 +62,7 @@ class TestContainerPostgresql():
         """
 
         container_name = settings['container_name']
+
         command_monit = (
         "busctl", "get-property", "org.freedesktop.systemd1", "/org/freedesktop/systemd1/unit/monit_2eservice",
         "org.freedesktop.systemd1.Unit", "ActiveState")
@@ -71,6 +74,7 @@ class TestContainerPostgresql():
 
         # check the return value
         monit_status = check_service().stdout.decode("utf-8")
+        logger.debug(monit_status)
 
         status = re.search(active_status, monit_status)
         assert status is not None
@@ -106,6 +110,7 @@ class TestContainerPostgresql():
         # stop postgresql
         stop_service = docker.bake("exec", "-i", container_name, "systemctl", "stop", service_name)
         logger.debug(stop_service)
+
         stop_service()
 
         tic_tac = 0
@@ -114,6 +119,7 @@ class TestContainerPostgresql():
         while (tic_tac < max_timeout) and (psql_is_up == False):
             # check if monit started postgresql
             time.sleep(1)
+
             check_service = docker.bake("exec", "-i", container_name, "systemctl", "status", service_name)
             logger.info("Check to see if {service} started after {time} seconds".format(service=service_name, time=tic_tac))
             logger.debug(check_service)
@@ -142,6 +148,7 @@ class TestContainerPostgresql():
         # kill postgresql
         stop_service = docker.bake("exec", "-i", container_name, "systemctl", "kill", service_name)
         logger.debug(stop_service)
+
         stop_service()
 
         tic_tac = 0
@@ -150,6 +157,7 @@ class TestContainerPostgresql():
         while (tic_tac < max_timeout) and (psql_is_up == False):
             # check if monit started postgresql
             time.sleep(1)
+
             check_service = docker.bake("exec", "-i", container_name, "systemctl", "status", service_name)
             logger.info("Check to see if {service} started after {time} seconds".format(service=service_name, time=tic_tac))
             logger.debug(check_service)
@@ -166,7 +174,7 @@ class TestContainerPostgresql():
 
     def test_no_error_monit_log(self, settings):
         """
-        Test to check that when running the container systemd starts influxdb and there is no error in the monit logs.
+        Test to check that, when running the container, systemd starts postgresql and there is no error in the monit logs.
         :param settings: settings of the container, e.g. container name, service name, etc.
         :return:
         """
@@ -183,7 +191,8 @@ class TestContainerPostgresql():
         restart_docker = docker.bake("start", container_name)
         logger.debug(restart_docker)
         restart_docker()
-        time.sleep(2)
+
+        time.sleep(3)
 
         # docker inspect --format='{{.State.Status}} container
         check_status = docker.bake("inspect", "--format='{{.State.StartedAt}}'", container_name)
@@ -194,7 +203,10 @@ class TestContainerPostgresql():
         get_monit_log = docker.bake("exec", container_name, "journalctl", "-u", "monit", "--since", last_started_date,
                                     "-p", "err", "-b")
         logger.debug(get_monit_log)
+
         monit_log = get_monit_log().stdout.decode("utf-8")
+        logger.debug(monit_log)
+
         assert re.search(no_error_status, monit_log) is not None
 
     def test_systemd_restarts_monit(self, settings):
@@ -211,6 +223,7 @@ class TestContainerPostgresql():
         # kill monit
         stop_service = docker.bake("exec", "-i", container_name, "systemctl", "kill", service_name)
         logger.debug(stop_service)
+
         stop_service()
 
         tic_tac = 0
@@ -219,6 +232,7 @@ class TestContainerPostgresql():
         while (tic_tac < max_timeout) and (monit_is_up == False):
             # check if systemd starts monit
             time.sleep(1)
+
             check_service = docker.bake("exec", "-i", container_name, "systemctl", "status", service_name)
             logger.info("Check to see if {service} started after {time} seconds".format(service=service_name, time=tic_tac))
             logger.debug(check_service)
@@ -231,6 +245,7 @@ class TestContainerPostgresql():
 
             except Exception as e:
                 tic_tac = tic_tac + 1
+
         assert monit_is_up == True
 
     def test_container_exposed_ports(self, settings):
@@ -245,7 +260,9 @@ class TestContainerPostgresql():
 
         check_ports = docker.bake("inspect", "--format='{{.Config.ExposedPorts}}'", container_name)
         logger.debug(check_ports)
+
         exposed_ports = check_ports().stdout.decode("utf-8")
+        logger.debug(exposed_ports)
 
         for port in ports:
             assert re.search(port, exposed_ports) is not None
@@ -269,6 +286,7 @@ class TestContainerPostgresql():
 
         # check the return value
         monit_restart = check_monit_restart().stdout.decode("utf-8")
+        logger.debug(monit_restart)
 
         status = re.search(restart_status, monit_restart)
         assert status is not None
